a14d2281335af1e6641e533919944a999744d792
ericg <ericgorr@gmail.com>
2019-11-11 14:25:36 -0500

    initial commit

---
+++ Compass/Location.swift
@@ -0,0 +1,64 @@
+//
+//  Compass.swift
+//  Compass
+//
+//  Created by ProgrammingWithSwift on 2019/10/06.
+//  Copyright © 2019 ProgrammingWithSwift. All rights reserved.
+//
+
+import Foundation
+import Combine
+import CoreLocation
+
+class Location: NSObject, ObservableObject, CLLocationManagerDelegate
+{
+    var objectWillChange = PassthroughSubject<String, Never>()
+    
+    @Published var location: String = "" {
+        didSet {
+            objectWillChange.send( location )
+        }
+    }
+    
+    @Published var occasionalLocation: String = ""
+    
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        self.locationManager = CLLocationManager()
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.setup()
+        
+        
+    }
+  
+    private var occasionalLocationCancellable: Cancellable? {
+        didSet {
+            oldValue?.cancel()
+        }
+    }
+    
+    deinit {
+        occasionalLocationCancellable?.cancel()
+    }
+    
+    private func setup() {
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+        
+        occasionalLocationCancellable = self.objectWillChange
+                                                .debounce(for: .seconds( 2 ), scheduler: DispatchQueue.main)
+                                                .sink(receiveValue: { (location) in
+                                                    print( "Received: \(location)" )
+                                                })
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation = locations.last!
+        let now             = Date().description
+        
+        self.location = "\(now): \(currentLocation.coordinate.latitude), \(currentLocation.coordinate.longitude)"
+    }
+}
